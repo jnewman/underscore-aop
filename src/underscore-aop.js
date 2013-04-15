d1d2eef3a062694ecdd29c91d3d3e842a097f0ce
Joshua Newman <joshua.l.newman@gmail.com>
2013-04-14 22:57:03 -0700

    Modified the dojo aspect library to work w/ lodash. Still need to test against underscore.

--- src/underscore-aop.js
+++ src/underscore-aop.js
@@ -1,15 +1,159 @@
 define(['lodash'], function (_) {
     'use strict';
-    return {
-        around: function (obj, methodName, advise) {
-            var origMethod = obj[methodName];
-            obj[methodName] = _.wrap(origMethod, advise);
-            return {
+    var nextId = 0;
+
+    function advise (dispatcher, type, advice, receiveArguments) {
+        var previous = dispatcher[type];
+        var around = type == 'around';
+        var signal;
+        if (around) {
+            var advised = advice(function () {
+                return previous.advice(this, arguments);
+            });
+            signal = {
                 remove: function () {
-                    obj[methodName] = origMethod;
-                    return origMethod;
+                    signal.cancelled = true;
+                },
+                advice: function (target, args) {
+                    return signal.cancelled ?
+                        previous.advice(target, args) : // cancelled, skip to next one
+                        advised.apply(target, args);    // called the advised function
                 }
             };
+        } else {
+            // create the remove handler
+            signal = {
+                remove: function () {
+                    var previous = signal.previous;
+                    var next = signal.next;
+                    if (!next && !previous) {
+                        delete dispatcher[type];
+                    } else {
+                        if (previous) {
+                            previous.next = next;
+                        } else {
+                            dispatcher[type] = next;
+                        }
+                        if (next) {
+                            next.previous = previous;
+                        }
+                    }
+                },
+                id: nextId++,
+                advice: advice,
+                receiveArguments: receiveArguments
+            };
         }
-    };
+        if (previous && !around) {
+            if (type === 'after') {
+                // add the listener to the end of the list
+                // note that we had to change this loop a little bit to workaround a bizarre IE10
+                // JIT bug
+
+                while (previous.next && (previous = previous.next)) {}
+                previous.next = signal;
+                signal.previous = previous;
+            } else if (type === 'before') {
+                // add to beginning
+                dispatcher[type] = signal;
+                signal.next = previous;
+                previous.previous = signal;
+            }
+        } else {
+            // around or first one just replaces
+            dispatcher[type] = signal;
+        }
+        return signal;
+    }
+
+    function aspect(type) {
+        return function (target, methodName, advice, receiveArguments) {
+            var existing = target[methodName], dispatcher;
+            if (!existing || existing.target !== target) {
+                // no dispatcher in place
+                target[methodName] = dispatcher = function () {
+                    // before advice
+                    var args = arguments;
+                    var before = dispatcher.before;
+                    while (before) {
+                        args = before.advice.apply(this, args) || args;
+                        before = before.next;
+                    }
+                    var results;
+                    // around advice
+                    if (dispatcher.around) {
+                        results = dispatcher.around.advice(this, args);
+                    }
+                    // after advice
+                    var after = dispatcher.after;
+                    var newResults;
+                    while (after && after.id < nextId) {
+                        if (after.receiveArguments) {
+                            newResults = after.advice.apply(this, args);
+                            // change the return value only if a new value was returned
+                            results = newResults === void 0 ? results : newResults;
+                        } else {
+                            results = after.advice.call(this, results, args);
+                        }
+                        after = after.next;
+                    }
+                    return results;
+                };
+                if (existing) {
+                    dispatcher.around = {advice: function (target, args) {
+                        return existing.apply(target, args);
+                    }};
+                }
+                dispatcher._unboundPointer = existing;
+                dispatcher.target = target;
+            }
+            var results = advise((dispatcher || existing), type, advice, receiveArguments);
+            advice = null;
+            return results;
+        };
+    }
+
+    aspect('before')(_, 'bind', function (func) {
+        var args = _.toArray(arguments);
+        // Keep a reference to the original function, so we can call it.
+        func._unboundPointer = func._unboundPointer || func;
+
+        args[0] = function () {
+            var dispatcher = _.find(this, function (value, key) {
+                return value._unboundPointer && value._unboundPointer === func;
+            });
+            return (dispatcher || func).apply(this, arguments);
+        };
+        return args;
+    });
+
+    // Leave the API open to either apsect(type, )
+    return _.extend(function (type) {
+        return aspect(type).apply(this, arguments);
+    }, {
+
+        /**
+         * @param {Object} target
+         * @param {string} methodName
+         * @param {Function.<Array|Arguments|undefined>} advice
+         * @return {Object.<{remove: Function, advice: Function}>}
+         */
+        before: aspect('before'),
+
+        /**
+         * @param {Object} target
+         * @param {string} methodName
+         * @param {Function.<Array|Arguments|undefined>} advice
+         * @return {Object.<{remove: Function, advice: Function}>}
+         */
+        around: aspect('around'),
+
+        /**
+         * @param {Object} target
+         * @param {string} methodName
+         * @param {Function.<Function>} advice
+         * @return {Object.<{remove: Function, advice: Function}>}
+         */
+        after: aspect('after')
+    });
 });
--- test/underscore-aop.js
+++ test/underscore-aop.js
@@ -1,22 +1,25 @@
-define(function (require) {
+define([
+    'mocha', 'chai',
+    'lodash',
+    'underscore-aop'
+], function (
+    mocha, chai,
+    _,
+    aop
+) {
     'use strict';
-
-    require('mocha');
-    var chai = require('chai');
     var assert = chai.assert;
-    var _ = require('lodash');
-    var aop = require('underscore-aop');
 
-    describe('Undescore AOP', function () {
-        var tested = null;
+    describe('underscore-aop', function () {
+        var subject = null;
 
         beforeEach(function () {
-            function Tested (props) {
+            function Subject (props) {
                 _.extend(this, props);
-                this._cid = Tested.prototype._cidCounter++;
+                this._cid = Subject.prototype._cidCounter++;
             }
 
-            _.extend(Tested.prototype, {
+            _.extend(Subject.prototype, {
                 idAttr: 'id',
                 _cidCounter: 0,
                 getId: function () {
@@ -26,22 +29,101 @@ define(function (require) {
                 setId: function (id) {
                     this[this.idAttr] = id;
                     return this;
+                },
+                /**
+                 *
+                 * @param {number...} number
+                 * @returns {number}
+                 */
+                sum: function (number) {
+                    //noinspection JSValidateTypes
+                    return _.reduce(arguments, function (total, current) {
+                        return total + Number(current);
+                    }, 0);
                 }
             });
 
-            tested = new Tested();
+            subject = new Subject();
         });
 
-        it('Can wrap a single method.', function () {
-            tested.setId(99);
+        afterEach(function () {
+            subject = null;
+        });
 
-            var handle = aop.around(tested, 'getId', function (orig) {
-                return 42 + orig.call(this);
+        it('can wrap a method', function () {
+            subject.setId(99);
+            assert.equal(subject.getId(), 99);
+
+            var handle = aop.around(subject, 'getId', function (orig) {
+                return function () {
+                    return 42 + orig.call(this);
+                };
             });
 
-            assert.equal(tested.getId(), 141);
+            assert.equal(subject.getId(), 141);
             handle.remove();
-            assert.equal(tested.getId(), 99);
+            assert.equal(subject.getId(), 99);
+        });
+
+        it('can inject functionality before a method is executed', function () {
+            assert.equal(subject.sum(1, 1), 2);
+            var handle = aop.before(subject, 'sum', function (number) {
+                var args = _.toArray(arguments);
+                args[0]++;
+                return args;
+            });
+
+            assert.equal(subject.sum(1, 1), 3);
+            handle.remove();
+            assert.equal(subject.sum(1, 1), 2);
+        });
+
+        it('can inject functionality after a method is executed', function () {
+            assert.equal(subject.sum(1, 1), 2);
+            var handle = aop.after(subject, 'sum', function (total) {
+                return total + 1;
+            });
+
+            assert.equal(subject.sum(1, 1), 3);
+            handle.remove();
+            assert.equal(subject.sum(1, 1), 2);
+        });
+
+        // Around is exempt from this.
+        // TODO: See if I can get this test to run in a reasonable amount of time.
+        it.skip('does not crash when aspecting the same method many times', function () {
+            // NOTE: chromium tends to overflow at ~10000, so massively exceeding.
+            var NUM_ASPECTS = 20000;
+
+            assert.equal(subject.sum(1, 1), 2);
+            var afterSum = function (total) {
+                return total + 1;
+            };
+
+            var handles = [];
+            var i = 0;
+
+            while (i++ < NUM_ASPECTS) {
+                //noinspection JSHint
+                handles.push(aop.after(subject, 'sum', afterSum));
+            }
+
+            assert.equal(subject.sum(1, 1), 2 + ITERS);
+            _.invoke(handles, 'remove');
+            assert.equal(subject.sum(1, 1), 2);
+        });
+
+        it('aspects a bound method', function () {
+            var getId = _.bind(subject.getId, subject);
+            assert.equal(subject.getId(), 0);
+
+            var handle = aop.after(subject, 'getId', function (id) {
+                return id + 1;
+            });
+
+            assert.equal(getId(), 1);
+            handle.remove();
+            assert.equal(getId(), 0);
         });
     });
 });
